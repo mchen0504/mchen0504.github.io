b251f0a31313bf3007e58a0b00ceb71f93960077
Michelle Chen <michellechen0504@gmail.com>
2022-02-17 16:08:01 -0800

    css fixes

--- js/index.js
+++ js/index.js
@@ -5,3 +5,32 @@ $(".hamburger-menu").on("click", () => {
 $(".nav-menu a").on("click", () => {
   $(".nav-menu").removeClass("menu-open");
 });
+
+// $(document).on("click", (event) => {
+//   let dropdown = $(".nav-menu");
+//   if (dropdown !== event.target && !dropdown.has(event.target).length) {
+//     $(".nav-menu").removeClass("menu-open");
+//   }
+// });
+
+// $(document).click((event) => {
+//   if (!$(event.target).closest(".nav-menu").length) {
+//     $(".nav-menu").removeClass("menu-open");
+//   }
+// });
+
+// $(document).on("click", function (event) {
+//   if (!$(event.target).closest(".nav-menu").length) {
+//     // Hide the menus.
+//     $(".nav-menu").removeClass("menu-open");
+//   }
+// });
+
+// $(document).click(function (e) {
+//   e.stopPropagation();
+//   let container = $(".nav-menu");
+
+//   if (container.has(e.target).length === 0) {
+//     $(".nav-menu").toggleClass("menu-open");
+//   }
+// });
